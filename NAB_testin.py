83b3779277dbee490a362492e9b6e8c027910b8b
AKgroup2000 <65353307+AKgroup2000@users.noreply.github.com>
2020-12-10 02:03:27 +0530

    Create NAB_testin.py

---
+++ NAB_testin.py
@@ -0,0 +1,155 @@
+"""
+read an interesting article about anomaly detection: https://iwringer.wordpress.com/2015/11/17/anomaly-detection-concepts-and-techniques/.
+I wanted to try a few of these techniques to better understand them. I searched an interesting dataset on Kaggle about anomaly detection with simple exemples. 
+I choose one exemple of NAB datasets (thanks for this datasets) and I implemented a few of these algorithms. The goal of this Notebook is just to implement these 
+techniques and understand there main caracteristics. Sometimes, they are not adapted to this datasets. I add some visualizations to understand what the algorithm 
+detect. Hope it can help some people. Notebook available (with Markov Chain) here: https://github.com/Vicam/Unsupervised_Anomaly_Detection"""
+
+# libraries
+#%matplotlib notebook
+
+import pandas as pd
+import numpy as np
+
+import matplotlib
+import seaborn
+import matplotlib.dates as md
+from matplotlib import pyplot as plt
+
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.covariance import EllipticEnvelope
+#from pyemma import msm # not available on Kaggle Kernel
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
+
+# some function for later
+
+# return Series of distance between each point and his distance with the closest centroid
+def getDistanceByPoint(data, model):
+    distance = pd.Series()
+    for i in range(0,len(data)):
+        Xa = np.array(data.loc[i])
+        Xb = model.cluster_centers_[model.labels_[i]-1]
+        distance.set_value(i, np.linalg.norm(Xa-Xb))
+    return distance
+
+# train markov model to get transition matrix
+def getTransitionMatrix (df):
+	df = np.array(df)
+	model = msm.estimate_markov_model(df, 1)
+	return model.transition_matrix
+
+def markovAnomaly(df, windows_size, threshold):
+    transition_matrix = getTransitionMatrix(df)
+    real_threshold = threshold**windows_size
+    df_anomaly = []
+    for j in range(0, len(df)):
+        if (j < windows_size):
+            df_anomaly.append(0)
+        else:
+            sequence = df[j-windows_size:j]
+            sequence = sequence.reset_index(drop=True)
+            df_anomaly.append(anomalyElement(sequence, real_threshold, transition_matrix))
+    return df_anomaly
+    
+    
+# 1 Data
+# 1.1 Extract data
+df = pd.read_csv(" * * * * * * NAB DATA SET Path * * * * * ")
+print(df.info())
+
+# check the timestamp format and frequence 
+print(df['timestamp'].head(10))
+
+# check the temperature mean
+print(df['value'].mean())
+
+# change the type of timestamp column for plotting
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+# change fahrenheit to °C (temperature mean= 71 -> fahrenheit)
+df['value'] = (df['value'] - 32) * 5/9
+# plot the data
+df.plot(x='timestamp', y='value')
+
+# 1.3 Feature engineering
+
+#   Extracting some useful features
+# the hours and if it's night or day (7:00-22:00)
+df['hours'] = df['timestamp'].dt.hour
+df['daylight'] = ((df['hours'] >= 7) & (df['hours'] <= 22)).astype(int)
+
+# the day of the week (Monday=0, Sunday=6) and if it's a week end day or week day.
+df['DayOfTheWeek'] = df['timestamp'].dt.dayofweek
+df['WeekDay'] = (df['DayOfTheWeek'] < 5).astype(int)
+# An estimation of anomly population of the dataset (necessary for several algorithm)
+outliers_fraction = 0.01
+
+# time with int to plot easily
+df['time_epoch'] = (df['timestamp'].astype(np.int64)/100000000000).astype(np.int64)
+
+# creation of 4 distinct categories that seem useful (week end/day week & night/day)
+df['categories'] = df['WeekDay']*2 + df['daylight']
+
+a = df.loc[df['categories'] == 0, 'value']
+b = df.loc[df['categories'] == 1, 'value']
+c = df.loc[df['categories'] == 2, 'value']
+d = df.loc[df['categories'] == 3, 'value']
+
+fig, ax = plt.subplots()
+a_heights, a_bins = np.histogram(a)
+b_heights, b_bins = np.histogram(b, bins=a_bins)
+c_heights, c_bins = np.histogram(c, bins=a_bins)
+d_heights, d_bins = np.histogram(d, bins=a_bins)
+
+width = (a_bins[1] - a_bins[0])/6
+
+ax.bar(a_bins[:-1], a_heights*100/a.count(), width=width, facecolor='blue', label='WeekEndNight')
+ax.bar(b_bins[:-1]+width, (b_heights*100/b.count()), width=width, facecolor='green', label ='WeekEndLight')
+ax.bar(c_bins[:-1]+width*2, (c_heights*100/c.count()), width=width, facecolor='red', label ='WeekDayNight')
+ax.bar(d_bins[:-1]+width*3, (d_heights*100/d.count()), width=width, facecolor='black', label ='WeekDayLight')
+
+plt.legend()
+plt.show()
+
+#2 Models
+# 2.1 Cluster only
+# Use for collective anomalies (unordered).
+"""
+We group together the usual combination of features. The points that are far from the cluster are points with usual combination of features.
+We consider those points as anomalies.
+"""
+
+# Take useful feature and standardize them
+data = df[['value', 'hours', 'daylight', 'DayOfTheWeek', 'WeekDay']]
+min_max_scaler = preprocessing.StandardScaler()
+np_scaled = min_max_scaler.fit_transform(data)
+data = pd.DataFrame(np_scaled)
+# reduce to 2 importants features
+pca = PCA(n_components=2)
+data = pca.fit_transform(data)
+# standardize these 2 new features
+min_max_scaler = preprocessing.StandardScaler()
+np_scaled = min_max_scaler.fit_transform(data)
+data = pd.DataFrame(np_scaled)
+
+# calculate with different number of centroids to see the loss plot (elbow method)
+n_cluster = range(1, 20)
+kmeans = [KMeans(n_clusters=i).fit(data) for i in n_cluster]
+scores = [kmeans[i].score(data) for i in range(len(kmeans))]
+fig, ax = plt.subplots()
+ax.plot(n_cluster, scores)
+plt.show()
+
+# Not clear for me, I choose 15 centroids arbitrarily and add these data to the central dataframe
+df['cluster'] = kmeans[14].predict(data)
+df['principal_feature1'] = data[0]
+df['principal_feature2'] = data[1]
+df['cluster'].value_counts()
+
+#plot the different clusters with the 2 main features
+fig, ax = plt.subplots()
+colors = {0:'red', 1:'blue', 2:'green', 3:'pink', 4:'black', 5:'orange', 6:'cyan', 7:'yellow', 8:'brown', 9:'purple', 10:'white', 11: 'grey', 12:'lightblue', 13:'lightgreen', 14: 'darkgrey'}
+ax.scatter(df['principal_feature1'], df['principal_feature2'], c=df["cluster"].apply(lambda x: colors[x]))
+plt.show()
